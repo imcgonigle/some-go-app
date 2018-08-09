3966ec8223df7c3fe17c0f081202e721fca95446
Ian McGonigle <ian.is.mcgonigle@gmail.com>
2018-08-09 12:44:02 -0600

    Change main.go to be a server that uses gorilla/mux for routing.

--- main.go
+++ main.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	fmt.Println("Hello world!")
+	fmt.Println("The program is starting")
+
+	r := mux.NewRouter()
+	r.HandleFunc("/monkeys", monkeyHandler)
+	r.HandleFunc("/", homeHandler)
+
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
+
+func monkeyHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "You've hit the monkey routes")
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
